1903a7981f8c710b1cd5ef7da23a3e35d3cdd75c
ogundeleoluwaferanmi35 <ogundeleoluwaferanmi35@users.noreply.github.com>
2026-09-16 11:33:42 +0100

    chore: remove unused scratch test file

--- packages/sdk/test-imports.ts
+++
@@ -1,2 +0,0 @@
-import { TransactionBuilder, Operation, xdr, Address, SorobanRpc, Contract, nativeToScVal } from '@stellar/stellar-sdk';
-console.log(!!TransactionBuilder, !!Operation, !!xdr, !!Address, !!SorobanRpc, !!Contract, !!nativeToScVal);
